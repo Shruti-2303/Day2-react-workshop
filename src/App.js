7562aced6ca904fac1a4e8f401bb0ae7a57e7e66
Shruti-2303 <sharmas23032001@gmail.com>
2023-01-21 21:48:32 +0530

    Fetching APIs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 function App() {
+
+  const [userDetails, setUserDetails] = useState();
+
+  const user = async () => {
+    try {
+      const userDetail = await axios.get("https://randomuser.me/api/");
+      setUserDetails(userDetail.data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(userDetails);
+  useEffect(() => {
+    user();
+  },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {userDetails && <img src={userDetails.picture.large}/>}
     </div>
   );
 }
